5fce8cdf07386b22a99c5e7c6ae74c9afb7d0406
MariaPolina <by.maria.polina@gmail.com>
2023-01-29 21:22:53 +0300

    feat: task complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+    const ones = ["", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"];
+    const tens = ["", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
+
+    let result = "";
+
+    if (number === 0) {
+        result += 'zero';
+    }
+
+    if (number >= 100) {
+        result += ones[Math.floor(number / 100)] + " hundred";
+        number = number % 100;
+    }
+
+    if (number >= 20) {
+        result += (result !== "" ? " " : "") + tens[Math.floor(number / 10)];
+        number = number % 10;
+    }
+
+    if (number > 0) {
+        result += (result !== "" ? " " : "") + ones[number];
+    }
+
+    return result;
+
 }
